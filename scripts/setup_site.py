77267faa6cc094b61906c062f04a071f40b1750d
l3ocifer <lpask001@gmail.com>
2024-10-03 22:36:43 -0400

    update imports to use absolute paths in scripts directory

--- scripts/setup_site.py
+++ scripts/setup_site.py
@@ -3,7 +3,7 @@
 import os
 import subprocess
 import logging
-from .customize_site import customize_site  # Corrected the import
+from scripts.customize_site import customize_site  # Corrected the import
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
